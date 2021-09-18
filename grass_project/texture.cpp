22a6326df69d9c369109672a5368a16d4cda2d10
Freemedude <daniel_g_hansen@hotmail.com>
2021-09-18 14:03:58 +0200

    Fix INVALID_VALUE on object label in Texture

--- grass_project/texture.cpp
+++ grass_project/texture.cpp
@@ -14,7 +14,7 @@ void Texture::bindTextureCubeMap() {
 	GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, textureID));
 }
 
-/* 
+/*
 	Loads a 2D texture from a specified file.
 
 	fileName: texture's filename
@@ -27,7 +27,7 @@ unsigned int Texture::loadTexture(std::string fileName, bool alpha) {
 	GLCall(glBindTexture(GL_TEXTURE_2D, textureID));
 
 	int width, height, nrChannels;
-	unsigned char* data = stbi_load(fileName.c_str(), &width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load(fileName.c_str(), &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		if (alpha) {
@@ -55,15 +55,18 @@ unsigned int Texture::loadTexture(std::string fileName, bool alpha) {
 }
 
 
-/* Loads a 2D texture from a specified file.
- * \param fileName - texture's filename
- * \param alpha - set to true if alpha channel should be read from texture
- * \return textureID
+/*
+	\brief Fuck
+
+	\param fileName: texture's filename
+	\param alpha: set to true if alpha channel should be read from texture
+
+	\return textureID
  */
-unsigned int Texture::loadTextureSingleChannel(const std::string& name, int perlinNoiseSize) {
+unsigned int Texture::loadTextureSingleChannel(const std::string &name, int perlinNoiseSize) {
 	GLCall(glGenTextures(1, &textureID));
-	GLCall(glObjectLabel(GL_TEXTURE, textureID, -1, name.data()));
 	GLCall(glBindTexture(GL_TEXTURE_2D, textureID));
+	GLCall(glObjectLabel(GL_TEXTURE, textureID, -1, name.c_str()));
 	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
 	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 	GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, perlinNoiseSize, perlinNoiseSize, 0, GL_RED, GL_FLOAT, 0));
@@ -75,7 +78,7 @@ unsigned int Texture::loadTextureSingleChannel(const std::string& name, int perl
 }
 
 
-void Texture::generateTexture(void* data, int width, int height, GLenum format) {
+void Texture::generateTexture(void *data, int width, int height, GLenum format) {
 	GLCall(glGenTextures(1, &textureID));
 	loadTextureData(data, width, height, format);
 }
@@ -86,7 +89,7 @@ void Texture::generateTexture(void* data, int width, int height, GLenum format) 
  * \param alpha - set to true if alpha channel should be read from texture
  * \return textureID
  */
-unsigned int Texture::loadTextureData(void* data, int width, int height, GLenum format) {
+unsigned int Texture::loadTextureData(void *data, int width, int height, GLenum format) {
 	GLCall(glBindTexture(GL_TEXTURE_2D, textureID));
 
 	GLCall(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_FLOAT, data));
@@ -123,7 +126,7 @@ unsigned int Texture::loadTextureCubeMap(std::vector<std::string> faces, bool al
 	int width, height, nrComponents;
 	for (unsigned int i = 0; i < faces.size(); i++)
 	{
-		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
+		unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
 		if (data)
 		{
 			if (alpha) {
