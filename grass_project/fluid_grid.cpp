5f6cb658873208233acec09d9cac1b66055746a0
Freemedude <daniel_g_hansen@hotmail.com>
2024-01-06 10:29:09 +0100

    Remove duplicate SWAP definition

--- grass_project/fluid_grid.cpp
+++ grass_project/fluid_grid.cpp
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 
 #define IX(i,j) ((i)+(N+2)*(j))
-#define SWAP(x0,x) {float * tmp=x0;x0=x;x=tmp;}
 #define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) {
 #define END_FOR }}
 
